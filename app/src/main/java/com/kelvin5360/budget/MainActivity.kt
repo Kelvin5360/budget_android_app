320b9019194bb9259a1480be8a8ec1a62c13c70f
kelvin5360@gmail.com <kelvin5360@gmail.com>
2018-12-30 12:32:42 +0800

    add method for button

--- app/src/main/java/com/kelvin5360/budget/MainActivity.kt
+++ app/src/main/java/com/kelvin5360/budget/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kelvin5360.budget
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +10,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun sendMessage(view: View) {
+        // Do something in response to button
+    }
+
 }

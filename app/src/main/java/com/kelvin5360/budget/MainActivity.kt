427f3ee74047c36176f4713bf51c8fb2f300ed27
kelvin5360@gmail.com <kelvin5360@gmail.com>
2018-12-30 15:20:58 +0800

    button do something

--- app/src/main/java/com/kelvin5360/budget/MainActivity.kt
+++ app/src/main/java/com/kelvin5360/budget/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.kelvin5360.budget
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +16,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun sendMessage(view: View) {
-        // Do something in response to button
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 
 }

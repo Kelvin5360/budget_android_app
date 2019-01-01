1d519db6c45ce192d45ca09b2faef7f7617f12b9
kelvin5360@gmail.com <kelvin5360@gmail.com>
2019-01-01 23:43:18 +0800

    empty layout to prepare adding buttons

--- app/src/main/java/com/kelvin5360/budget/DisplayMessageActivity.kt
+++ app/src/main/java/com/kelvin5360/budget/DisplayMessageActivity.kt
@@ -2,11 +2,16 @@ package com.kelvin5360.budget
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class DisplayMessageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = message
+        }
     }
 }
